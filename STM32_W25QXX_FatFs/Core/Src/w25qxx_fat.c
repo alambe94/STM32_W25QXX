6188866dcd6d9d58b813a0185f70dc4c2dc36f97
Your Name <you@example.com>
2020-07-31 09:37:47 +0530

    cube update

--- STM32_W25QXX_FatFs/Core/Src/w25qxx_fat.c
+++ STM32_W25QXX_FatFs/Core/Src/w25qxx_fat.c
@@ -1,4 +1,5 @@
 #include "stm32f4xx_hal.h"
+#include "w25qxx.h"
 #include "fatfs.h"
 
 
@@ -7,22 +8,20 @@ uint8_t buffer[_MAX_SS]; /* a work buffer for the f_mkfs() */
 void W25Q_FAT_Main()
     {
 
-
     FRESULT res;
-    uint8_t tx[] = "This is STM32 working with FatFs using W25QXX as storage medium";
+    uint8_t tx[] = "This is STM32 working with FatFs using W25QXX as storage medium. jndjknjk sdjnfskvjndjk";
     uint8_t rx[100] = "0";
 
     UINT cnt;
 
-    //W25QXX_Erase_Chip();
-
     res = f_mount(&USERFatFS, (TCHAR const*) USERPath, 0);
 
     /*only first time*/
-    //res = f_mkfs((TCHAR const*) USERPath, FM_ANY, 0, buffer, sizeof(buffer));
-    //res = f_open(&USERFile, "STM32.TXT", FA_CREATE_ALWAYS | FA_WRITE);
-    //res = f_write(&USERFile, tx, sizeof(tx), &cnt);
-    //res = f_close(&USERFile);
+    W25QXX_Erase_Chip();
+    res = f_mkfs((TCHAR const*) USERPath, FM_ANY, 0, buffer, sizeof(buffer));
+    res = f_open(&USERFile, "STM32.TXT", FA_CREATE_ALWAYS | FA_WRITE);
+    res = f_write(&USERFile, tx, sizeof(tx), &cnt);
+    res = f_close(&USERFile);
 
     res = f_open(&USERFile, "STM32.TXT", FA_READ);
     res = f_read(&USERFile, rx, sizeof(rx), &cnt);
